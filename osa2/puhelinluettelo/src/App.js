cd5b6ad6422ab48db324b7395c9ff52dfb712681
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-06-12 14:22:50 +0300

    tehtävä 2.12

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,12 +5,8 @@ import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons"
 
 const App = () => {
-  /*const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);*/
+
+  const baseURL = "http://localhost:3001/persons";
 
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
@@ -19,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-    .get("http://localhost:3001/persons")
+    .get(baseURL)
     .then(response => {
       setPersons(response.data)
     })
@@ -36,9 +32,14 @@ const App = () => {
       name: newName,
       number: newNumber,
     };
-    setPersons(persons.concat(obj));
-    setNewName('');
-    setNewNumber('');
+    
+    axios
+      .post(baseURL, obj)
+      .then(response => {
+        setPersons(persons.concat(response.data));
+        setNewName('');
+        setNewNumber('');
+      });
   };
 
   const handleNameChange = (event) => {
