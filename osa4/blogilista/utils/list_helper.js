8ed38ec3f0a65f14137394e6534dfc5c7a162e69
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-06 16:03:57 +0300

    tehtävä 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -9,8 +9,18 @@ const totalLikes = (blogs) => {
     const likes = blogs.map(blog => blog.likes)
     return likes.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    }
+    const likes = blogs.map(blog => blog.likes)
+    const index = likes.indexOf(Math.max(...likes))
+    return blogs[index]
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
