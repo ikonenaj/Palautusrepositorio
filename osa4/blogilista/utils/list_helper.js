b9e97c8997902926a0d58aedc61a19ee2fad9d5f
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-06 15:49:31 +0300

    tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    const likes = blogs.map(blog => blog.likes)
+    return likes.reduce(reducer, 0)
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
