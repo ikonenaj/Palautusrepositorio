5a5ea71dbdac23b54bd8c3901e2ebaa3362cc791
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-10 17:25:09 +0300

    tehtävä 4.17

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1, id: 1 })
+    response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
@@ -21,10 +26,6 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
-})
-
-usersRouter.get('/', async (request, response) => {
-    return response.json(await User.find({}))
 })
 
 module.exports = usersRouter
