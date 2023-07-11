eccdabac83d2d69977812ece7943bd7e69bd449c
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-11 13:09:01 +0300

    tehtävä 4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,11 +18,13 @@ blogsRouter.get('/', async (request, response) => {
   
   blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
-    const users = await User.find({})
-    const user = users[0]
-
-    const blog = new Blog({
+    const newBlog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
@@ -21,7 +32,7 @@ blogsRouter.get('/', async (request, response) => {
       user: user._id
     })
 
-    const savedBlog = await blog.save()
+    const savedBlog = await newBlog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
     response.status(201).json(savedBlog)
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,8 +1,12 @@
 const errorHandler = (error, request, response, next) => {
     console.log(error.message)
   
-    if (error.name === 'ValidationError') {
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message })
+    } else if (error.name ===  'JsonWebTokenError') {
+      return response.status(400).json({ error: 'token missing or invalid' })
     }
   
     next(error)
