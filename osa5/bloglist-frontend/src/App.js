8d41a759b64e4f9be6930bb5c8db33b79f734e78
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-13 15:38:40 +0300

    tehtävä 5.1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -27,6 +27,8 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      loginService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -35,12 +37,44 @@ const App = () => {
     }
   }
 
-  return (
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const blogList = () => (
     <div>
       <h2>blogs</h2>
+      {user.name} logged in <br/><br/>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      {!user && loginForm()}
+      {user && blogList()}
     </div>
   )
 }
--- osa5/bloglist-frontend/src/services/login.js
+++ osa5/bloglist-frontend/src/services/login.js
@@ -1,9 +1,16 @@
 import axios from 'axios'
 const baseUrl = '/api/login'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const login = async credentials => {
   const response = await axios.post(baseUrl, credentials)
   return response.data
 }
 
-export default { login }
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { login, setToken }
