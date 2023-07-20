1c0ce1d6b462b469cd7a23d2485eedc03804be47
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-20 17:04:42 +0300

    tehtävä 6.24

--- osa6/query-anecdotes/src/notificationContext.js
+++ osa6/query-anecdotes/src/notificationContext.js
@@ -1,7 +1,5 @@
 import { createContext, useContext, useReducer } from "react"
 
-let timeout = null
-
 const notificationReducer = (state, action) => {
     switch (action.type) {
         case 'SET':
