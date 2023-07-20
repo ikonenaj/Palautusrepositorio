ab43df791f0a197ca40c2d2907d46a0863283887
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-20 14:00:24 +0300

    tehtävä 6.20

---
+++ osa6/query-anecdotes/src/requests.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = async () => {
+    const response = await axios.get(baseUrl)
+    console.log(response.data);
+    return response.data
+}
