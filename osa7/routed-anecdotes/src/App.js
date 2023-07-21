ce1aa6f28b760248db03d50cd7476cf7f59a89f6
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-21 14:01:50 +0300

    tehtävä 7.5

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -75,6 +75,12 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const reset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -91,7 +97,7 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button><button type="reset" onClick={reset}>reset</button>
       </form>
     </div>
   )
