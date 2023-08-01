36e476a6f4113a7e3b89e3324cec159794a76e43
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-08-01 14:37:56 +0300

    tehtävät 7.20 ja 7.21

--- osa7/bloglist/frontend-redux/src/components/BlogForm.js
+++ osa7/bloglist/frontend-redux/src/components/BlogForm.js
@@ -1,14 +1,9 @@
-import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setNotification } from '../reducers/notificationReducer'
 import PropTypes from 'prop-types'
 import { Form, Button } from 'react-bootstrap'
 
 const BlogForm = ({ createBlog }) => {
-  //const [title, setTitle] = useState('')
-  //const [author, setAuthor] = useState('')
-  //const [url, setUrl] = useState('')
-
   const dispatch = useDispatch()
 
   const addBlog = async (event) => {
@@ -27,47 +22,6 @@ const BlogForm = ({ createBlog }) => {
       dispatch(setNotification(error.response.data.error, 'error', 5))
     }
   }
-
-  /*return (
-    <div>
-      <h2>create new</h2>
-      <form onSubmit={addBlog}>
-        <div>
-          title:
-          <input
-            id="title"
-            type="text"
-            value={title}
-            name="title"
-            onChange={(event) => setTitle(event.target.value)}
-          />
-        </div>
-        <div>
-          author:
-          <input
-            id="author"
-            type="text"
-            value={author}
-            name="author"
-            onChange={(event) => setAuthor(event.target.value)}
-          />
-        </div>
-        <div>
-          url:
-          <input
-            id="url"
-            type="text"
-            value={url}
-            name="url"
-            onChange={(event) => setUrl(event.target.value)}
-          />
-        </div>
-        <button id="submit-button" type="submit">
-          create
-        </button>
-      </form>
-    </div>
-  )*/
 
   return (
     <Form onSubmit={addBlog}>
