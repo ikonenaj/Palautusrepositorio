10f1268fdac750af10560c33d988fe76ff4814d2
Antti-Jussi Ikonen <antti-jussi.ikonen@aalto.fi>
2023-08-13 14:05:35 +0300

    tehtävä 9.5

--- osa9/first-steps/index.ts
+++ osa9/first-steps/index.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+// import { calculateBmi } from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_request, response) => {
     response.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (request, response) => {
+    const { height, weight } = request.query;
+    if (isNaN(Number(height)) || isNaN(Number(weight))) {
+        response.status(400).json({ error: 'malformatted parameters' });
+    }
+    response.json({
+        height: height,
+        weight: weight,
+        bmi: calculateBmi(Number(height), Number(weight))
+    })
+})
 
 const PORT = 3001;
 
