40701cd69203b4bcf05b5068e1c72935422f0731
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-08-21 16:39:36 +0300

    tehtävä 9.18

--- osa9/flight-diary/frontend/src/App.tsx
+++ osa9/flight-diary/frontend/src/App.tsx
@@ -1,27 +1,65 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useState, useEffect } from "react";
-
-interface Diary {
-  id: string;
-  date: string;
-  weather: string;
-  visibility: string;
-  comment: string;
-}
-
-type NonSensitiveDiary = Omit<Diary, 'comment'>
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry, Visibility, Weather } from './types';
 
 const App = () => {
-  const [diaries, setDiaries] = useState<NonSensitiveDiary[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newComment, setNewComment] = useState('');
+  const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    axios.get<NonSensitiveDiary[]>('http://localhost:3001/api/diaries').then(response => {
+    axios.get<NonSensitiveDiaryEntry[]>('http://localhost:3001/api/diaries').then(response => {
       setDiaries(response.data);
     });
   }, []);
 
+  const diaryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const diaryToAdd: NewDiaryEntry = {
+      date: newDate,
+      visibility: newVisibility as Visibility,
+      weather: newWeather as Weather,
+      comment: newComment
+    };
+      axios.post<DiaryEntry>('http://localhost:3001/api/diaries', diaryToAdd).then(response => {
+        const { comment, ...newDiaryEntry } = response.data;
+        setDiaries(diaries.concat(newDiaryEntry));
+        setNewDate('');
+        setNewVisibility('');
+        setNewWeather('');
+        setNewComment('');
+      })
+      .catch(e => {
+        const error = e as AxiosError;
+        if (error.response) {
+          const errorMessage = error.response.data;
+          setError(errorMessage as string);
+          setTimeout(() => {
+            setError('');
+          }, 5000);
+        }
+      });
+    }
+
+
   return (
     <div className="App">
+      <h2>Add new entry</h2>
+      <p style={{color: 'red'}}>{error}</p>
+      <form onSubmit={diaryCreation}>
+        date
+        <input value={newDate} onChange={event => setNewDate(event.target.value)} /><br />
+        visibility
+        <input value={newVisibility} onChange={event => setNewVisibility(event.target.value)} /><br />
+        weather
+        <input value={newWeather} onChange={event => setNewWeather(event.target.value)} /><br />
+        comment
+        <input value={newComment} onChange={event => setNewComment(event.target.value)} /><br />
+        <button type='submit'>add</button>
+      </form>
       <h2>Diary entries</h2>
       {diaries.map(entry => 
         <div key={entry.id}>
