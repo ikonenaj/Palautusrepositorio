f63a1de46d5fc963359eabb0fd4589dd5b9fad25
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-08-22 12:00:13 +0300

    tehtävä 9.19

--- osa9/flight-diary/frontend/src/App.tsx
+++ osa9/flight-diary/frontend/src/App.tsx
@@ -7,16 +7,18 @@ const App = () => {
   const [newVisibility, setNewVisibility] = useState('');
   const [newWeather, setNewWeather] = useState('');
   const [newComment, setNewComment] = useState('');
-  const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
-    axios.get<NonSensitiveDiaryEntry[]>('http://localhost:3001/api/diaries').then(response => {
+    axios.get<DiaryEntry[]>('http://localhost:3001/api/diaries').then(response => {
+      console.log(response.data);
       setDiaries(response.data);
     });
   }, []);
 
   const diaryCreation = (event: React.SyntheticEvent) => {
+    console.log(event);
     event.preventDefault();
     const diaryToAdd: NewDiaryEntry = {
       date: newDate,
@@ -25,12 +27,13 @@ const App = () => {
       comment: newComment
     };
       axios.post<DiaryEntry>('http://localhost:3001/api/diaries', diaryToAdd).then(response => {
-        const { comment, ...newDiaryEntry } = response.data;
-        setDiaries(diaries.concat(newDiaryEntry));
+        setDiaries(diaries.concat(response.data));
         setNewDate('');
         setNewVisibility('');
         setNewWeather('');
         setNewComment('');
+        const form = document.getElementById('diary-form') as HTMLFormElement
+        form.reset();
       })
       .catch(e => {
         const error = e as AxiosError;
@@ -49,13 +52,40 @@ const App = () => {
     <div className="App">
       <h2>Add new entry</h2>
       <p style={{color: 'red'}}>{error}</p>
-      <form onSubmit={diaryCreation}>
-        date
-        <input value={newDate} onChange={event => setNewDate(event.target.value)} /><br />
-        visibility
-        <input value={newVisibility} onChange={event => setNewVisibility(event.target.value)} /><br />
-        weather
-        <input value={newWeather} onChange={event => setNewWeather(event.target.value)} /><br />
+      <form id='diary-form' onSubmit={diaryCreation}>
+        date{' '}
+        <input type='date' onChange={event => setNewDate(event.target.value)}/><br />
+        <div>
+          visibility{' '}
+          <input type='radio' id='great' name='visibility' onChange={() => setNewVisibility('great')} />
+          <label htmlFor='great'>great</label>
+
+          <input type='radio' id='good' name='visibility' onChange={() => setNewVisibility('good')} />
+          <label htmlFor='good'>good</label>
+
+          <input type='radio' id='ok' name='visibility' onChange={() => setNewVisibility('ok')} />
+          <label htmlFor='ok'>ok</label>
+
+          <input type='radio' id='poor' name='visibility' onChange={() => setNewVisibility('poor')} />
+          <label htmlFor='poor'>poor</label>
+        </div>
+        <div>
+          weather
+          <input type='radio' id='sunny' name='weather' onChange={() => setNewWeather('sunny')} />
+          <label htmlFor='sunny'>sunny</label>
+
+          <input type='radio' id='rainy' name='weather' onChange={() => setNewWeather('rainy')} />
+          <label htmlFor='rainy'>rainy</label>
+
+          <input type='radio' id='cloudy' name='weather' onChange={() => setNewWeather('cloudy')} />
+          <label htmlFor='cloudy'>cloudy</label>
+
+          <input type='radio' id='stormy' name='weather' onChange={() => setNewWeather('stormy')} />
+          <label htmlFor='stormy'>stormy</label>
+
+          <input type='radio' id='windy' name='weather' onChange={() => setNewWeather('windy')} />
+          <label htmlFor='windy'>windy</label>
+        </div>
         comment
         <input value={newComment} onChange={event => setNewComment(event.target.value)} /><br />
         <button type='submit'>add</button>
