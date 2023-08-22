bef5abb6a53fcf6d87b5d6b547e52b529a5bf736
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-08-22 16:07:54 +0300

    tehtävä 9.22

--- osa9/patientor/backend/src/types.ts
+++ osa9/patientor/backend/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnose {
+export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
@@ -10,7 +10,46 @@ export enum Gender {
     Other = "other"
 }
 
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+
+}
+  
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
 }
 
 export interface Patient {
@@ -23,5 +62,6 @@ export interface Patient {
     entries: Entry[];
 }
 
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id' | 'entries'>;
